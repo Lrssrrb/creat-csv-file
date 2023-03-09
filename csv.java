36b8ff68334a5735a2454f71295e9440ebaa2e8b
Luckiraj Sisodiya <sisodiyarana07@gmail.com>
2023-03-09 11:47:20 +0530

    create csv

---
+++ csv.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+  
+  
+@RestController
+public class Controller {
+
+  @Autowired
+	HttpServletResponse httpServletResponse;
+  
+  @GetMapping
+  public void genrateCsv() {
+        try {	
+          httpServletResponse.setContentType("text/csv");
+          httpServletResponse.setHeader("Content-Disposition", "attachment; filename=customersFileName.csv");
+          CsvBeanWriter csvBeanWriter = new CsvBeanWriter(httpServletResponse.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+
+          String[] HTMLHeadingElement = {"ONE","TWO","THREE"};
+          String[] classPro = {"one","two","three"};
+
+          csvBeanWriter.writeHeader(HTMLHeadingElement);
+
+          ClassCsv zegoOtpRequest = new ClassCsv("1","2L","jkb");
+
+          csvBeanWriter.write(zegoOtpRequest, classPro);
+          csvBeanWriter.close();
+
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+
+    }
